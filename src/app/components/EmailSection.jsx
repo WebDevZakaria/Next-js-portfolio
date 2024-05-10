af59da79f5397710db7aee1f9fdb41811677e697
zakaria bouregbi <zakariabouregbidev@gmail.com>
2024-05-10 17:22:52 +0100

    edit email secrtion

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -50,7 +50,7 @@ const EmailSection = () => {
                 id="email"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="jacob@google.com"
+                placeholder="zakariabouregbidev@gmail.com"
               />
             </div>
             <div className="mb-6">
@@ -66,7 +66,7 @@ const EmailSection = () => {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
+                placeholder="ask me anything"
               />
             </div>
             <div className="mb-6">
