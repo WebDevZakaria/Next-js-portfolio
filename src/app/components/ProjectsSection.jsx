047ff64629d7b19cb1c1c8dbb48a6a7ed5795855
zakaria bouregbi <zakariabouregbidev@gmail.com>
2024-05-10 16:47:52 +0100

    add project

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -2,6 +2,11 @@
 import { useState } from 'react';
 
 import Handymen from '../../../public/images/projects/handymen.png'
+
+import Zedproperty from '../../../public/images/projects/zedproperty.png'
+
+
+
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Ecommerce from '../../../public/images/projects/ecommerce.png'
 import Link from "next/link";
@@ -118,6 +123,58 @@ const ProjectsSection = () => {
 
 
           </li>
+
+          
+        {/* ZedProperty  */}
+
+
+        <li>
+
+<div>
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{backgroundImage: `url(${Zedproperty.src})`, backgroundSize: "cover" }}
+      >
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+          <Link
+            href='https://github.com/WebDevZakaria/RealEstate-nextjs-app'
+            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            target="_blank"
+          >
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+          </Link>
+        
+            <Link
+            href='https://zedproperty.vercel.app/'
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            target="_blank"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+          </Link>
+         
+        </div>
+      </div>
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">ZedProperty Website
+            </h5>
+        <p className="text-[#ADB7BE]">
+        ZedProperty is a web application designed to simplify the process
+         of finding and renting properties. Built with Next.js and MongoDB
+        </p>
+      </div>
+    </div>
+
+
+          </li>
+
+
+
+
+
+
+
+
+
 
  {/* ecommerce django and react */}
           <li>
@@ -250,9 +307,11 @@ const ProjectsSection = () => {
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
             target="_blank"
           >
+
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+
           </Link>
-        
+
             <Link
             href='https://dashing-douhua-c7111c.netlify.app/'
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
