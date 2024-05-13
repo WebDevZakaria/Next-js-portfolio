c0e5b6e9e3a46a1c10d5bd9823f31b378ad58817
zakaria bouregbi <zakariabouregbidev@gmail.com>
2024-05-13 13:12:49 +0100

    add another project

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -5,6 +5,8 @@ import Handymen from '../../../public/images/projects/handymen.png'
 
 import Zedproperty from '../../../public/images/projects/zedproperty.png'
 
+import Allres from '../../../public/images/projects/allres.png'
+
 
 
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
@@ -453,6 +455,56 @@ const ProjectsSection = () => {
 
 
 </li>
+
+{/* RESTaurant searching app  */}
+<li>
+
+<div>
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{backgroundImage: `url(${Allres.src})`, backgroundSize: "cover" }}
+      >
+        
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+          <Link
+            href='https://github.com/WebDevZakaria/Next-js-App'
+            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            target="_blank"
+          >
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+          </Link>
+        
+            <Link
+            href='https://brrestaurant.vercel.app/'
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            target="_blank"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+          </Link>
+         
+        </div>
+      </div>
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">All Restaurant
+            </h5>
+        <p className="text-[#ADB7BE]">Website To search for the restaurant you want </p> 
+      </div>
+    </div>
+
+
+</li>
+
+
+
+
+
+
+
+
+
+
+
+
 
       </ul>
       
